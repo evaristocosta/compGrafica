279fdfdfc11e91feb36e2c3bb01d8695f85719a6
Lucas Costa <lucascosta74@gmail.com>
2019-10-02 17:39:41 -0300

    continuacao do trab2

--- trabalho2.c
+++ trabalho2.c
@@ -3,35 +3,70 @@
 #include <stdlib.h>
 #include <GL/glut.h>
 
-GLfloat ctrlpoints[7][3]  = {
+GLfloat ctrlpoints[7][3] = {
         { -4.0, -4.0, 0.0}, 
         { -2.0, 4.0, 0.0}, 
         {2.0, -4.0, 0.0}, 
         {4.0, 5.0, 0.0}, 
         {4.0, 4.0, 0.0}}; 
+GLint qtde_pontos = 0;
 
+void DesenhaEixos(void) {
+	// eixos secundarios
+	glColor3f(0.5f,0.5f,0.5f);	// cor cinza
+    int i = 0;
+    for (i = 0; i < 10; i++) {
+        // verticais
+        glBegin(GL_LINES);
+            glVertex2f((GLfloat) i-5.0,-5.0);
+            glVertex2f((GLfloat) i-5.0,5.0);
+        glEnd();
+
+        // horizontais
+        glBegin(GL_LINES);
+            glVertex2f(-5.0,(GLfloat) i-5.0);
+            glVertex2f(5.0,(GLfloat) i-5.0);
+        glEnd();
+    }
+
+    glColor3f(1.0f,1.0f,1.0f);	// cor branca
+    // eixos principais
+	glBegin(GL_LINES);
+		glVertex2f(0,-5.0);
+		glVertex2f(0,5.0);
+	glEnd();
+
+	glBegin(GL_LINES);
+		glVertex2f(-5.0,0);
+		glVertex2f(5.0,0);
+	glEnd();
+}
 
 void Desenha(void) {
     int i;
 
+    glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
     glClear(GL_COLOR_BUFFER_BIT);
 
     // Cor da curva
     glClearColor(0.0, 0.0, 0.0, 0.0);
+
     glShadeModel(GL_FLAT);
     glMap1f(GL_MAP1_VERTEX_3, 0.0, 1.0, 3, 4, &ctrlpoints[0][0]);
     glEnable(GL_MAP1_VERTEX_3);
 
-    glColor3f(1.0, 1.0, 1.0);
+    DesenhaEixos();
+	
     glBegin(GL_LINE_STRIP);
         for (i = 0; i <= 30; i++) 
             glEvalCoord1f((GLfloat) i/30.0);
     glEnd();
-    /* The following code displays the control points as dots. */
+    /* The following code displays the control points as a line. */
     glPointSize(5.0);
     glColor3f(1.0, 1.0, 0.0);
     glBegin(GL_LINE_STRIP);
-        for (i = 0; i < 4; i++) 
+        for (i = 0; i < qtde_pontos; i++) 
             glVertex3fv(&ctrlpoints[i][0]);
     glEnd();
     glFlush();
@@ -54,6 +89,7 @@ void reshape(int w, int h)
 
 int main(int argc, char** argv)
 {
+    qtde_pontos = 3;
     glutInit(&argc, argv);
     glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize (500, 500);
